1151a07ccddeecfcae408c6ab7f1fa03e3b74e79
Makhlouf Ait Laziz <makhloufaitlaziz@gmail.com>
2021-08-15 15:28:13 +0200

    Closing toggle

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".navbar-hamburger").classList.toggle("show");
 });
+
+document.querySelector(".container").addEventListener("click", () => {
+  document.querySelector(".navbar-hamburger").classList.remove("show");
+});
